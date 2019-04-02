0d9ab2d3d3b615f5a1383261afc4eb1e69741d78
Raphael Zahner <rz271994@gmail.com>
2019-04-02 21:57:34 +0200

    Make some Visual Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$('#exampleModal').on('shown.bs.modal', function (e) {
-    $('#modalbody').html('<iframe src="pdfjs/web/viewer.html?file=Demo.pdf"  width="100%" frameborder="0"></iframe>')
+$('#pdfViewerModal').on('shown.bs.modal', function (e) {
+    $('#modalbody').html('<iframe src="pdfjs/web/viewer.html?file=compressed.tracemonkey-pldi-09.pdf"  width="100%" frameborder="0"></iframe>')
 })
-$('#exampleModal').on('hidden.bs.modal', function (e) {
+$('#pdfViewerModal').on('hidden.bs.modal', function (e) {
     $('#modalbody').html('')
 })
