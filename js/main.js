e2ab996d22b8759da01c2ca90818f238b17ec00d
Raphael Zahner <rz271994@gmail.com>
2019-04-03 22:10:31 +0200

    Fix the Code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('#pdfViewerModal').on('shown.bs.modal', function (e) {
-    $('#modalbody').html('<iframe src="pdfjs/web/viewer.html?file=compressed.tracemonkey-pldi-09.pdf"  width="100%" frameborder="0"></iframe>')
-})
+    $('#modalbody').html('<iframe src="pdfjs/web/viewer.html?file=compressed.tracemonkey-pldi-09.pdf"  width="100%" frameborder="0"></iframe>');
+});
 $('#pdfViewerModal').on('hidden.bs.modal', function (e) {
-    $('#modalbody').html('')
-})
+    $('#modalbody').html('');
+});
